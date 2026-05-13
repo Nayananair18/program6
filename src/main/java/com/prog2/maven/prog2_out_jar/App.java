1ca5f4880bd4187dd93952f9f3d97b4d3f21c780
Nayananair18 <nayananair241@gmail.com>
2026-05-13 09:07:45 +0530

    InitialCommit

---
+++ src/main/java/com/prog2/maven/prog2_out_jar/App.java
@@ -0,0 +1,13 @@
+package com.prog2.maven.prog2_out_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
